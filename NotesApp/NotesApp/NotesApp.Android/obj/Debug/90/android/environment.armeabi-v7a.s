d277dafbdb0098f6412402641959afef04ce1d96
Lucas Fonseca <lucasjosefonsecasilva@gmail.com>
2020-10-27 23:18:21 -0300

    Criando telas para exbir e salvar notas.

--- NotesApp/NotesApp/NotesApp.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ NotesApp/NotesApp/NotesApp.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"99db8428-45d3-4ebc-bacc-3b85c9877112"
+	.asciz	"482fd323-158c-4b76-8846-e71ed3156f34"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
