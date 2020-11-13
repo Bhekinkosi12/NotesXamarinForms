4653a848717ab1e4b1cf31ce05b6db3b8d2edd5b
Lucas Fonseca <lucasjosefonsecasilva@gmail.com>
2020-11-13 17:11:24 -0300

    Implementando SQLlite(sqlite-net-pcl)

--- NotesApp/NotesApp/NotesApp.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ NotesApp/NotesApp/NotesApp.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"482fd323-158c-4b76-8846-e71ed3156f34"
+	.asciz	"fff16078-2e34-487b-b9a0-671b4b0b2c13"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
